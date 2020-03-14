60ff2da5ddcb89dac3b0c7429a840d3edd0e53d2
aschern <aschernyavskiy@gmail.com>
2020-03-14 16:42:08 +0300

    __pycache__ removed

--- tools/src/annotation_task_si.py
+++ tools/src/annotation_task_si.py
@@ -1,7 +1,7 @@
 import sys
 import src.annotation as an
 
-class AnnotationTaskSI(Annotation)
+class AnnotationTaskSI(Annotation):
 
     def __init__(self, label=None, start_offset = None, end_offset=None): #, article_id=None):
         
